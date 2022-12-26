d520a8a624f3b31fb6b6bed5e48d9656c9c23a37
krishnamandhane <kkmandhane@gmail.com>
2022-12-26 22:57:02 +0530

    changes to file java

--- file2.java
+++ file2.java
@@ -4,7 +4,7 @@ public static void main(){
    
    int x = 6;
    int y = 7;
-system.out.pintln("Hello World"+x);
-system.out.pintln("Hello World"+y);
+system.out.pintln("Hello World"+x+"y:"+y);
+
 }
 }
