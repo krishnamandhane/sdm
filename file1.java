da7a852d79b055f473cef038853568a2f5c4538c
krishnamandhane <kkmandhane@gmail.com>
2022-12-26 22:36:39 +0530

    initial commit

---
+++ file1.java
@@ -0,0 +1,8 @@
+class Git{
+
+public static void main(){
+   
+   int x =5;
+system.out.pintln("Hello World"+x);
+}
+}
---
+++ file2.java
@@ -0,0 +1,8 @@
+class Git{
+
+public static void main(){
+   
+   int x =6;
+system.out.pintln("Hello World"+x);
+}
+}
